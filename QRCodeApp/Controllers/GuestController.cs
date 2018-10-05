ee8528ed8c052593682274db613fbd782b9248fd
Jeffrey Pucaputan <kylenexus@gmail.com>
2018-10-05 10:22:57 +0800

    upload files

---
+++ QRCodeApp/Controllers/GuestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+using QRCodeApp.Models;
+
+namespace QRCodeApp.Controllers
+{
+    public class GuestController : Controller
+    {
+        // GET: Guest
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Guest guest)
+        {
+            
+            return RedirectToAction("GetQRCode",guest);
+        }
+
+        [HttpGet]
+        public ActionResult GetQRCode()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult GetQRCode(Guest guest)
+        {
+            string googleApi = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=";
+            ViewBag.JsonGuest = JsonConvert.SerializeObject(guest);
+            ViewBag.ImgUrl = googleApi + ViewBag.JsonGuest;
+            
+
+            return View(guest);
+        }
+    }
+}
---
+++ QRCodeApp/Models/Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRCodeApp.Models
+{
+    public class Guest
+    {
+        public int id { get; set; }
+        public string EmployeeId { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
